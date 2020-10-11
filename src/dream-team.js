a1e6dbb073e048d5d116a6e258057e851734fd5d
Anna Startseva <a.v.starceva@gmail.com>
2020-10-12 01:13:44 +0600

    feat: add solution of task Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+
+    if (!(Array.isArray(members)) || members.length === 0) {
+        return false;
+    } else {
+        let abreviatura = members.filter(element => typeof element === 'string').map(element => element.trim().charAt(0).toUpperCase()).sort().reduce((acc, element) => acc += element, '');
+
+        //console.log(abreviatura);
+        return abreviatura;
+    }
+}
