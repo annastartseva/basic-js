35e23e337e4a9035209c1670e1059481b1eb5ca5
Anna Startseva <a.v.starceva@gmail.com>
2020-10-30 05:23:38 +0600

    feat: add solution for task vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,75 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(reverseVal) {
+        this.reverseVal = reverseVal;
+    }
+
+    encrypt(message, key) {
+        if (this.message === '' || this.key === '') {
+            throw new "Error";
+        }
+        let encryptMessage = "";
+        //perebiraem message
+        for (var i = 0, j = 0; i < message.length; i++) {
+            //toUpperCase
+            let mesUpCase = message[i].toUpperCase();
+            // if A-Z
+            if (mesUpCase.match(/[A-Z]/i)) {
+                let keyUpCase = key[j % key.length].toUpperCase();
+                j++;
+                let mesCharCode = mesUpCase.charCodeAt(0);
+                let keyCharCode = keyUpCase.charCodeAt(0);
+                let sumMesKey = ((mesCharCode - 65) + (keyCharCode - 65));
+                encryptMessage += String.fromCharCode((sumMesKey % 26) + 65);
+            } else {
+                encryptMessage = `${encryptMessage}${mesUpCase}`;
+            }
+        }
+
+        function reverseFunc(encryptMessage) {
+            return encryptMessage.split("").reverse().join("");
+        }
+        if (this.reverseVal === false) {
+            return reverseFunc(encryptMessage);
+        } else {
+            return encryptMessage;
+        }
+    }
+
+    decrypt(message, key) {
+        if (this.message === '' || this.key === '') {
+            throw new "Error";
+        }
+        let encryptMessage = "";
+        //perebiraem message
+        for (var i = 0, j = 0; i < message.length; i++) {
+            //toUpperCase
+            let mesUpCase = message[i].toUpperCase();
+            // if A-Z
+            if (mesUpCase.match(/[A-Z]/i)) {
+                let keyUpCase = key[j % key.length].toUpperCase();
+                j++;
+                let mesCharCode = mesUpCase.charCodeAt(0);
+                let keyCharCode = keyUpCase.charCodeAt(0);
+                let sumMesKey = (mesCharCode - 65) - (keyCharCode - 65);
+                if (sumMesKey < 0) { sumMesKey = (mesCharCode - 65) + 26 - (keyCharCode - 65); }
+                let perem = ((sumMesKey) % 26) + 65;
+                encryptMessage += String.fromCharCode(perem);
+            } else {
+                encryptMessage = `${encryptMessage}${mesUpCase}`;
+            }
+        }
+
+        function reverseFunc(encryptMessage) {
+            return encryptMessage.split("").reverse().join("");
+        }
+        if (this.reverseVal === false) {
+            return reverseFunc(encryptMessage);
+        } else {
+            return encryptMessage;
+        }
+    }
 }
 
 module.exports = VigenereCipheringMachine;
